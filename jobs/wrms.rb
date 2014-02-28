afbb6b1858b8f54306f9c379b21281e830669205
Florent Delannoy <florent@catalyst.net.nz>
2014-02-28 13:45:25 +1300

    added clock, changed css

--- jobs/wrms.rb
+++ jobs/wrms.rb
@@ -11,6 +11,7 @@ SCHEDULER.every '30m', :first_in => 0 do |job|
 	wrms = WRMS.new
 	wrms.login(user_id,flubber)
 	p wrms.cookie
+	send_event('wrms_weekly', { value: wrms.weekly_timesheet })
 end
 
 
@@ -19,7 +20,7 @@ class WRMS
 		@http = Net::HTTP.new("wrms.catalyst.net.nz", 443)
 		@http.use_ssl = true
 		@http.verify_mode = OpenSSL::SSL::VERIFY_PEER
-		@http.set_debug_output($stdout)
+#		@http.set_debug_output($stdout)
 	end
 	def login(user_id,password)
 		begin
@@ -27,12 +28,39 @@ class WRMS
 			request.set_form_data({'user_id' => user_id, 'password' => password})
 			response = @http.request(request)
 			response_parsed = JSON.parse(response.body)
-			@cookie = response_parsed["response"]["auth_cookie_value"]
+			@cookie = response_parsed["response"]["auth_cookie_name"] + '=' + response_parsed["response"]["auth_cookie_value"]
 		rescue
 			@cookie = false
 		end
 	end
 	def cookie
 		return @cookie
+	end
+	def weekly_timesheet
+		return unless @cookie
+		week_start = Date.parse('Monday')
+		monday = week_start.strftime("%Y-%m-%d")
+		sunday = (week_start + 6).strftime("%Y-%m-%d")
+		p monday+':'+sunday
+		request = Net::HTTP::Get.new("/api2/report?" +  URI.encode_www_form({
+			'created_date'=>monday+':'+sunday,
+			'worker'=>'2583',
+			'report_type'=>'timesheet',
+			'page_size'=>'500',
+			'page_no'=>'1',
+			'display_fields'=>'request_id,hours,worker_fullname',
+			'order_by'=>'timesheet_id',
+			'order_direction'=>'desc'
+		}))
+		request["Cookie"] = @cookie
+		response = @http.request(request)
+		r = JSON.parse(response.body)
+		total = 0.0
+		r['response']['results'].each do |ts|
+			total += ts['hours']
+		end
+		p r
+		p total
+		return total
 	end
 end
